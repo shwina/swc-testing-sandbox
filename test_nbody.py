deacb97ebc88d436545673891e694d8b45275cf0
Ashwin Srinath <ashwinsrnth@gmail.com>
2015-03-20 09:51:30 -0400

    fix the tests

--- test_nbody.py
+++ test_nbody.py
@@ -6,11 +6,10 @@ class TestNBody:
         offset_momentum(BODIES['sun'])
 
     def test_momentum_conservation(self):
-        print report_energy(), report_momentum()
+        P1 = report_momentum()
         advance(0.01, 10)
         P2 = report_momentum()
-        print report_energy(), report_momentum()
-        assert_allclose(P1, 2*P2, rtol=0.01)
+        assert_allclose(P1, P2, rtol=0.01)
 
     def test_energy_conservation(self):
         E1 = report_energy()
